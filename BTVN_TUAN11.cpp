703fd6d77e87f12e22397ad3a9207618a0947450
linhvu2602work-web <linhvu2602.work@gmail.com>
2026-05-27 22:10:36 +0700

    Update BTVN_TUAN11.cpp

--- BTVN_TUAN11.cpp
+++ BTVN_TUAN11.cpp
@@ -43,11 +43,9 @@ Node* cayLechPhai() {
     Node* B = taoNode('B');
     Node* C = taoNode('C');
     Node* D = taoNode('D');
-
     A->right = B;
     B->right = C;
     C->right = D;
-
     return A;
 }
 Node* cayZigZag() {
@@ -55,16 +53,13 @@ Node* cayZigZag() {
     Node* B = taoNode('B');
     Node* C = taoNode('C');
     Node* D = taoNode('D');
-
     A->left = B;
     B->right = C;
     C->left = D;
-
     return A;
 }
 Node* cayBieuThuc() {
     Node* chia = taoNode('/');
-
     Node* cong = taoNode('+');
     Node* d = taoNode('d');
 
@@ -115,16 +110,11 @@ void truoc(Node* root) {
 }
 void giua(Node* root) {
     if(root != NULL) {
-
         if(root->left != NULL)
             cout << "(";
-
         giua(root->left);
-
         cout << root->data;
-
         giua(root->right);
-
         if(root->right != NULL)
             cout << ")";
     }
@@ -139,91 +129,58 @@ void sau(Node* root) {
 int main() {
     // CAY DAY DU
     cout << "CAY DAY DU\n";
-
     Node* daydu = cayDayDu();
-
     cout << "Duyet truoc : ";
     duyetTruoc(daydu);
-
     cout << "\nDuyet giua  : ";
     duyetGiua(daydu);
-
     cout << "\nDuyet sau   : ";
     duyetSau(daydu);
-
     cout << "\n\n";
-
    
     // CAY LECH TRAI
-    
     cout << "CAY LECH TRAI\n";
-
     Node* trai = cayLechTrai();
-
     cout << "Duyet truoc : ";
     duyetTruoc(trai);
-
     cout << "\nDuyet giua  : ";
     duyetGiua(trai);
-
     cout << "\nDuyet sau   : ";
     duyetSau(trai);
-
     cout << "\n\n";
-
     
     // CAY LECH PHAI
-    
     cout << "CAY LECH PHAI\n";
-
     Node* phai = cayLechPhai();
-
     cout << "Duyet truoc : ";
     duyetTruoc(phai);
-
     cout << "\nDuyet giua  : ";
     duyetGiua(phai);
-
     cout << "\nDuyet sau   : ";
     duyetSau(phai);
-
     cout << "\n\n";
-
     
     // CAY ZIGZAG
     
     cout << "CAY ZIGZAc\n";
-
     Node* zigzag = cayZigZag();
-
     cout << "Duyet truoc : ";
     duyetTruoc(zigzag);
-
     cout << "\nDuyet giua  : ";
     duyetGiua(zigzag);
-
     cout << "\nDuyet sau   : ";
     duyetSau(zigzag);
-
     cout << "\n\n";
-
     
     // CAY BIEU THUC
-    
     cout << "CAY BIEU THUC\n";
-
     Node* bt = cayBieuThuc();
-
     cout << "Tien to : ";
     truoc(bt);
-
     cout << "\nTrung to  : ";
     giua(bt);
-
     cout << "\nHau to : ";
     sau(bt);
-
     cout << "\n";
-
     return 0;
 }
